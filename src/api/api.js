112cc3feb48e97ef9d1d073609a6c19064eaaf9c
Sorawid Mingmolee <bossxlp@gmail.com>
2022-08-24 20:22:41 +0700

    change path path

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const host = 'https://back-cyan.vercel.app'
+export const host = 'https://backended-deploy.vercel.app'
 
 export const api = axios.create({
     baseURL: `${host}/api/v1`
